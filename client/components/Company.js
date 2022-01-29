a5c398665b9e96640ba56c5e16a1dbd3b9410721
Russel McMillan <rfmcmillan@gmail.com>
2022-01-29 12:27:22 -0500

    added enabled and test-publisher toggles

--- client/components/Company.js
+++ client/components/Company.js
@@ -1,21 +1,62 @@
-import React from "react";
-import { Typography, Paper } from "@material-ui/core";
+import React, { useState } from "react";
+import { FormControlLabel, Paper, Switch, Typography } from "@material-ui/core";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import data from "../../data.json";
 
 const Company = () => {
+  const {
+    name,
+    settings: { general },
+  } = data;
+
+  const [enabled, setEnabled] = useState(general.enabled);
+  const [testPublisher, setTestPublisher] = useState(general.testPublisher);
+
   const theme = useTheme();
   const useStyles = makeStyles({
-    name: { fontSize: 20, margin: 10, padding: 10 },
-    root: { width: "33vw" },
+    enabled: { margin: 5 },
+    name: { fontSize: 20, padding: 10 },
+    root: { width: "33vw", margin: 10 },
   });
   const classes = useStyles();
+
+  const handleEnabledToggle = (event) => {
+    setEnabled(event.target.checked);
+  };
+
+  const handleTestPublisherToggle = (event) => {
+    setTestPublisher(event.target.checked);
+  };
 
   return (
     <Paper className={classes.root}>
       <Typography className={classes.name} variant="h2">
-        {data.name}
+        {name}
       </Typography>
+
+      <FormControlLabel
+        className={classes.enabled}
+        control={
+          <Switch
+            checked={enabled}
+            onChange={handleEnabledToggle}
+            name="enabled"
+          ></Switch>
+        }
+        label={enabled === true ? "Enabled" : "Disabled"}
+      />
+      <FormControlLabel
+        control={
+          <Switch
+            checked={testPublisher}
+            onChange={handleTestPublisherToggle}
+            name="test-publisher"
+          ></Switch>
+        }
+        label={
+          testPublisher === true ? "Test Publisher" : "Not a Test Publisher"
+        }
+      />
     </Paper>
   );
 };
