020a8bdba2cdb3667ef7fde13fb4c19e6cd3ff17
Russel McMillan <rfmcmillan@gmail.com>
2022-01-29 14:29:51 -0500

    added revenue slider

--- client/components/Company/index.js
+++ client/components/Company/index.js
@@ -1,18 +1,27 @@
 import React, { useState } from "react";
 import {
+  FormControl,
+  FormLabel,
   FormControlLabel,
   Grid,
   Paper,
+  Radio,
+  RadioGroup,
+  Slider,
   Switch,
   Typography,
 } from "@material-ui/core";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import data from "../../../data.json";
 
+function valuetext(value) {
+  return `${value}%`;
+}
+
 const Company = () => {
   const {
     name,
-    settings: { general },
+    settings: { general, revenue },
   } = data;
 
   const [enabled, setEnabled] = useState(general.enabled);
@@ -22,9 +31,17 @@ const Company = () => {
   const theme = useTheme();
 
   const useStyles = makeStyles({
+    accountType: { margin: ".5rem" },
+    accountTypeLabel: { fontSize: ".75rem" },
     enabled: { margin: ".5rem" },
     name: { fontSize: "1.5rem", padding: 10 },
-    root: { width: "33vw", margin: 10 },
+    revenueShareContain: { margin: ".5rem" },
+    revenueShareLabel: {
+      color: theme.palette.text.secondary,
+      fontSize: ".75rem",
+    },
+    revenueShareSlider: { width: 250 },
+    root: { width: "33vw", margin: 10, padding: ".5rem" },
     testPublisher: { margin: ".5rem" },
     timezoneContain: { margin: ".5rem" },
     timezoneLabel: { color: theme.palette.text.secondary },
@@ -32,15 +49,18 @@ const Company = () => {
   const classes = useStyles();
 
   const handleEnabledToggle = (event) => {
-    setEnabled(event.target.checked);
+    const { target } = event;
+    setEnabled(target.checked);
   };
 
   const handleTestPublisherToggle = (event) => {
-    setTestPublisher(event.target.checked);
+    const { target } = event;
+    setTestPublisher(target.checked);
   };
 
   const handleAccountTypeChange = (event) => {
-    setAccountType(event.target.checked);
+    const { target } = event;
+    setAccountType(target.value);
   };
 
   return (
@@ -60,7 +80,6 @@ const Company = () => {
         }
         label={enabled === true ? "Enabled" : "Disabled"}
       />
-
       <FormControlLabel
         className={classes.testPublisher}
         control={
@@ -87,6 +106,42 @@ const Company = () => {
           </Typography>
         </Grid>
       </Grid>
+      <FormControl className={classes.accountType} component="fieldset">
+        <FormLabel className={classes.accountTypeLabel} component="legend">
+          Account Type
+        </FormLabel>
+        <RadioGroup
+          aria-label="account-type"
+          name="account-type"
+          value={accountType}
+          onChange={handleAccountTypeChange}
+        >
+          <FormControlLabel value="CTZero" control={<Radio />} label="CTZero" />
+          <FormControlLabel value="CTLite" control={<Radio />} label="CTLite" />
+          <FormControlLabel value="CTPlus" control={<Radio />} label="CTPlus" />
+          <FormControlLabel value="CTPro" control={<Radio />} label="CTPro" />
+        </RadioGroup>
+      </FormControl>
+      <div className={classes.revenueShareContain}>
+        <Typography
+          id="revenue-share-slider"
+          className={classes.revenueShareLabel}
+          gutterBottom
+        >
+          Revenue Share
+        </Typography>
+        <Slider
+          className={classes.revenueShareSlider}
+          defaultValue={revenue.publisherShare}
+          getAriaValueText={valuetext}
+          aria-labelledby="revenue-share-slider"
+          valueLabelDisplay="auto"
+          step={0.1}
+          marks={true}
+          min={0.1}
+          max={0.9}
+        />
+      </div>
     </Paper>
   );
 };
