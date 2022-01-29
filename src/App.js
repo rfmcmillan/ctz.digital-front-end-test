d19cd484bb74155f3e67d3898436daeb78b79b28
Russel McMillan <rfmcmillan@gmail.com>
2022-01-29 07:27:25 -0500

    App.js and index.js files added

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { ThemeProvider, createTheme } from "@material-ui/core/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#264653",
+      contrastText: "#fafafa",
+    },
+    secondary: {
+      main: "#2A9D8F",
+      contrastText: "#264653",
+    },
+    text: {
+      primary: "#264653",
+      secondary: "#2A9D8F",
+    },
+    info: {
+      main: "#2A9D8F",
+    },
+  },
+  typography: {
+    fontFamily: ["Poppins", "sans-serif"].join(","),
+  },
+});
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <div></div>
+    </ThemeProvider>
+  );
+};
+
+export default App;
