bff8bd45a08122181c34b57443dc860ed0727e75
Russel McMillan <rfmcmillan@gmail.com>
2022-02-03 11:21:00 -0500

    basic terms component started

--- client/components/Company/Terms.js
+++ client/components/Company/Terms.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Paper, Typography } from "@material-ui/core";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 
 const Terms = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
   const theme = useTheme();
 
   const useStyles = makeStyles({
@@ -19,9 +20,57 @@ const Terms = () => {
   });
   const classes = useStyles();
 
+  const onFileChange = (event) => {
+    // Update the state
+    setSelectedFile({ selectedFile: event.target.files[0] });
+  };
+
+  const onFileUpload = () => {
+    // Create an object of formData
+    const formData = new FormData();
+
+    // Update the formData object
+    formData.append("myFile", selectedFile, selectedFile.name);
+
+    // Details of the uploaded file
+    console.log(selectedFile);
+
+    // Request made to the backend api
+    // Send formData object
+    // axios.post("api/uploadfile", formData);
+  };
+
+  const fileData = () => {
+    if (selectedFile) {
+      return (
+        <div>
+          <h2>File Details:</h2>
+
+          <p>File Name: {selectedFile.name}</p>
+
+          <p>File Type: {selectedFile.type}</p>
+
+          <p>Last Modified: {selectedFile.lastModifiedDate.toDateString()}</p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <br />
+          <h4>Choose before Pressing the Upload button</h4>
+        </div>
+      );
+    }
+  };
+
   return (
     <Paper elevation={5} className={classes.root}>
       <Typography>Terms of Service</Typography>
+      <div>
+        <input type="file" onChange={onFileChange} />
+        <button onClick={onFileUpload}>Upload</button>
+      </div>
+      {fileData()}
     </Paper>
   );
 };
